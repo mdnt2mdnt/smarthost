9fe51a62f5f87a8501783b9eaee28e3ee26260a4
mooringniu <mooringniu@3297a6ac-2f0d-a868-55ea-5c34fc32a2df>
2012-12-31 08:12:01 +0000

    description update

--- trunk/src/make.cmd
+++ trunk/src/make.cmd
@@ -1,10 +1,10 @@
 @echo off
-PATH=C:\Windows\Microsoft.NET\Framework\v3.5;D:\Programs\NSIS
+PATH=C:\Windows\Microsoft.NET\Framework\v3.5;C:\Program Files\NSIS
 @del /f /q ..\obj\SmartHost.dll ..\obj\SmartHost.exe
 title Making SmartHost Plugin
 REM ..\tools\setVersion.exe 1.0.2.3 SmartHost.cs install.nsi
 @echo on
-@csc /o /w:1 /out:..\obj\Smarthost.dll /target:library SmartHost.cs /reference:"D:\Programs\Fiddler2\Fiddler.exe" /nologo /utf8output
+@csc /o /w:1 /out:..\obj\Smarthost.dll /target:library SmartHost.cs /reference:"C:\Program Files\Fiddler\Fiddler.exe" /nologo /utf8output
 @IF "%ERRORLEVEL%" NEQ "0" ( 
     @color f4
     @echo "Compile Dll Error"
