a1516ed629c2824cc0eb588d469fb20214d3c35c
mooringniu <mooringniu@3297a6ac-2f0d-a868-55ea-5c34fc32a2df>
2013-11-25 03:22:59 +0000

    ip detecting tools

---
+++ trunk/tools/src/getip/getIP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+
+
+class IPReport{
+    public static void Main()
+    {
+        string _wifiIP = String.Empty;
+        string _lanIP  = String.Empty;
+        foreach(NetworkInterface NI in NetworkInterface.GetAllNetworkInterfaces()) {
+            if(NI.NetworkInterfaceType==NetworkInterfaceType.Wireless80211) {
+                foreach(UnicastIPAddressInformation IP in NI.GetIPProperties().UnicastAddresses) {
+                    if (IP.Address.AddressFamily==System.Net.Sockets.AddressFamily.InterNetwork && IP.IsDnsEligible && !NI.Description.Contains("Virtual")) {
+                        _wifiIP = IP.Address.ToString();
+                        break;
+                    }
+                }
+            } else if(NI.NetworkInterfaceType==NetworkInterfaceType.Ethernet) {
+                foreach(UnicastIPAddressInformation IP in NI.GetIPProperties().UnicastAddresses) {
+                    if (IP.Address.AddressFamily==System.Net.Sockets.AddressFamily.InterNetwork && IP.IsDnsEligible && !NI.Description.Contains("Virtual")) {
+                        //USB NetWork Adapter will Here
+                        if(NI.Description.Contains("Wireless") && NI.Description.Contains("802.")){
+                            _wifiIP = IP.Address.ToString();
+                        }else{
+                            _lanIP = IP.Address.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        System.Console.WriteLine("\nNetwork Adapter Addresses : " + _wifiIP + " " + _lanIP + "\n");
+    }
+}
