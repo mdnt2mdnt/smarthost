7e882c389445437f67ad701ac865c3c39264dc7e
mooringniu <mooringniu@3297a6ac-2f0d-a868-55ea-5c34fc32a2df>
2013-12-09 16:12:22 +0000

    share on wechat functionality add

---
+++ trunk/package/share.js
@@ -0,0 +1,56 @@
+var shareData = {
+	img_url : 'https://code.google.com/p/smarthost/logo',
+	img_width : '640',
+	img_height : '640',
+	link : 'http://smarthost.sinaapp.com/help.php?id=1',
+	desc : '???????',
+	title : 'SmartHost',
+	content : '#SmartHost#???????',
+	url : 'http://smarthost.sinaapp.com/help.php?id=1'
+};
+function shareFriend() {
+	WeixinJSBridge.invoke("sendAppMessage", {
+		appid : shareData.appid,
+		img_url : shareData.img_url,
+		img_width : shareData.img_width,
+		img_height : shareData.img_height,
+		link : shareData.link,
+		desc : shareData.desc,
+		title : shareData.title
+	}, function (a) {});
+}
+function shareTimeline() {
+	var title = shareData.title;
+	if (title.indexOf(shareData.desc) == -1) {
+		title += ":" + shareData.desc;
+	}
+	WeixinJSBridge.invoke("shareTimeline", {
+		img_url : shareData.img_url,
+		img_width : shareData.img_width,
+		img_height : shareData.img_height,
+		link : shareData.link,
+		desc : shareData.desc,
+		title : title
+	}, function (a) {});
+}
+function shareWeibo() {
+	WeixinJSBridge.invoke("shareWeibo", {
+		content : shareData.content,
+		url : shareData.url || ' '
+	}, function (a) {});
+}
+(function () {
+    if (/MicroMessenger/i.test(navigator.userAgent)) {
+        document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+            WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+                shareFriend();
+            });
+            WeixinJSBridge.on('menu:share:timeline', function (argv) {
+                shareTimeline();
+            });
+            WeixinJSBridge.on('menu:share:weibo', function (argv) {
+                shareWeibo();
+            });
+        }, false);
+    }
+})();
